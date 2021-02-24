564d5246f2190f962cb3741bec09f0f89e8f4781
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2021-02-25 03:10:57 +0900

    updated getselectedcamera

--- resources/js/app.js
+++ resources/js/app.js
@@ -39,22 +39,18 @@ Vue.component('ptzcontrol-component', require('./components/PTZControlComponent.
 Vue.component('systeminfo-component', require('./components/SystemInfoComponent.vue').default);
 Vue.component('cameralist-component', require('./components/CameraListComponent.vue').default);
 
-Vue.mixin({
-    methods: {
-        getSelectedCamera: function() {
-            var selectedCameraId = this.$store.state.selectedCameraId;
-            if (selectedCameraId == "")
-                return undefined;
-            
-            var cameras = this.$store.state.cameras;
-            var index = cameras.findIndex(_ => _.cameraId === selectedCameraId);
-            if (index == -1)
-                return undefined;
+Vue.getSelectedCamera = function() {
+    var selectedCameraId = this.$store.state.selectedCameraId;
+    if (selectedCameraId == "")
+        return undefined;
+    
+    var cameras = this.$store.state.cameras;
+    var index = cameras.findIndex(_ => _.cameraId === selectedCameraId);
+    if (index == -1)
+        return undefined;
 
-            return cameras[index];
-        }
-    }
-}) 
+    return cameras[index];
+}
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
